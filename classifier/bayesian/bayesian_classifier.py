6510be30ba66afacf13af9b1a634a53022b845ee
Jonas Myrlund <myrlund@gmail.com>
2012-11-21 21:04:03 +0100

    Added dataset loader. Almost done with training part of NBC, classification barely begun.

--- classifier/bayesian/bayesian_classifier.py
+++ classifier/bayesian/bayesian_classifier.py
@@ -1,3 +1,5 @@
+from classifier.classifier import Classifier
+
 class BayesianClassifier(Classifier):
     """docstring for BayesianClassifier"""
     def __init__(self):
@@ -22,7 +24,31 @@ class BayesianClassifier(Classifier):
     def total_attribute_count_in_category(self, category):
         """Total number of attributes for a category."""
         if category in self.categories:
-            return float(self.categories[category])
+            return float(self.categories[category]["a"])
+        else:
+            return 0.0
+    
+    def training_sets_for_category(self, category):
+        if category in self.categories:
+            return float(self.categories[category]["t"])
+        else:
+            return 0.0
+    
+    def category_probability(self, category, attributes):
+        """Gets the probability for a category given a set of attributes."""
+        p_category = self.training_sets_for_category(category) / self.training_count
+        p_doc = self
+        return p_category
+    
+    def category_probabilities(self, attributes):
+        """Gets probabilities for each of the possible categories, given the set of attributes."""
+        import operator
+        
+        probs = {}
+        for category in self.categories.keys():
+            probs[category] = self.category_probability(category, attributes)
+        
+        return sorted(probs.iteritems(), key=operator.itemgetter(1)).reverse()
     
     def train(self, training_instance):
         """Adds the experience of a training instance to the classifier."""
@@ -43,20 +69,21 @@ class BayesianClassifier(Classifier):
             
             # Increment number of categories having this attribute
             self.attributes[attribute]["c"][training_instance.category] += 1
+            
+            # Increment number of attributes for this category
+            self.categories[training_instance.category]["a"] += 1
 
     def bootstrap_instance(self, training_instance):
         """Sets up internal structures for handling a training instance."""
         
         if training_instance.category not in self.categories:
             self.categories[training_instance.category] = {}
-            self.categories[training_instance.category]["t"] = 0
-            self.categories[training_instance.category]["a"] = 0
+            self.categories[training_instance.category] = dict(t=0, a=0)
         
         for attribute in training_instance.attributes:
             
             if attribute not in self.attributes:
-                self.attributes[attribute]["t"] = 0
-                self.attributes[attribute]["c"] = {}
+                self.attributes[attribute] = dict(t=0, c={})
         
             if training_instance.category not in self.attributes[attribute][CATEGORIES]:
                 self.attributes[attribute]["c"][training_instance.category] = 0
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # encoding: utf8
 
+from classifier.dataset_loader import load_dataset
+from classifier.bayesian.bayesian_classifier import BayesianClassifier
+
+test_factor = 1.0/4
+
 def main():
-    print "Main event loop"
-    pass
+    data_set = load_dataset('./datasets/yeast.txt')
+    set_split = int(len(data_set)*test_factor)
+    test_set     = data_set[:set_split]
+    training_set = data_set[set_split:]
+    
+    classifier = BayesianClassifier()
+    for training_instance in training_set:
+        classifier.train(training_instance)
 
 if __name__ == '__main__':
     main()
